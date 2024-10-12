2225ae713d54d378210c038bfa3c0ed5e4b88038
xu zhang <shinhwagk@outlook.com>
2024-10-12 13:37:58 +0800

    0.0.14

--- mysqlping_server.ts
+++ mysqlping_server.ts
@@ -1,10 +1,10 @@
 import { parseArgs } from "util";
 import mysql from 'mysql2/promise';
-import { getTimestamp, sleep, parseConnectionString, logger, type MysqlDsn, type MysqlPingClient } from './mysqlping_lib'
+import { getTimestamp, sleep, parseConnectionString, logger, type MysqlDsn } from './mysqlping_lib'
 
 class MysqlPing {
-    private initState = false
-    private isPing = false
+    private initState = false;
+    private isPing = false;
     private connectionPool: mysql.Pool;
 
     constructor(private readonly md: MysqlDsn, private readonly floor: boolean) {
@@ -17,73 +17,45 @@ class MysqlPing {
         });
     }
 
-    getName() {
-        return this.md.name
-    }
-
-    getAddr() {
-        return `${this.md.host}:${this.md.port}`
-    }
-
-    getIsPing() {
-        return this.isPing
-    }
-
-    setIsPing(state: boolean) {
-        this.isPing = state
-    }
-
-    private async init() {
+    async init() {
         if (this.initState) return;
         const connection = await this.connectionPool.getConnection();
         try {
             await connection.execute('CREATE DATABASE IF NOT EXISTS `mysql_ping`');
             await connection.execute('CREATE TABLE IF NOT EXISTS `mysql_ping`.`heartbeat` (ping_name VARCHAR(10) PRIMARY KEY, ping_timestamp INT)');
-            this.initState = true
+            this.initState = true;
         } finally {
             connection.release();
         }
     }
 
     async ping(timestamp: number) {
-        if (this.floor) {
-            logger("start ping use floor.")
-            await this.ping_floor(timestamp)
-        } else {
-            logger("start ping use non-floor.")
-            await this.ping_nonfloor(timestamp)
-        }
-    }
-
-    private async ping_floor(timestamp: number) {
-        if (!this.initState) {
-            await this.init()
-        }
-
+        if (!this.initState) await this.init();
         const connection = await this.connectionPool.getConnection();
         try {
-            await connection.execute('REPLACE INTO mysql_ping.heartbeat(ping_name, ping_timestamp) VALUES (?, ?)', [MP_FOLLOWER_NAME, timestamp]);
-            logger(`Ping inserted for name: ${this.md.name} timestamp: ${timestamp}`);
-        } finally {
-            connection.release();
-        }
-    }
-
-    private async ping_nonfloor(timestamp: number) {
-        const connection = await this.connectionPool.getConnection();
-        try {
-            await connection.execute('select 1');
+            if (this.floor) {
+                logger("start ping use floor.");
+                await connection.execute('REPLACE INTO mysql_ping.heartbeat(ping_name, ping_timestamp) VALUES (?, ?)', [MP_FOLLOWER_NAME, timestamp]);
+            } else {
+                logger("start ping use non-floor.");
+                await connection.execute('SELECT 1');
+            }
             logger(`Ping executed for name: ${this.md.name} timestamp: ${timestamp}`);
         } finally {
             connection.release();
         }
     }
+
+    getName() { return this.md.name; }
+    getAddr() { return `${this.md.host}:${this.md.port}`; }
+    getIsPing() { return this.isPing; }
+    setIsPing(state: boolean) { this.isPing = state; }
 }
 
 const { values } = parseArgs({
     args: Bun.argv,
     options: {
-        "follower-ping": { type: 'string', },
+        "follower-ping": { type: 'string' },
         "source-dsns": { type: 'string' },
         "ping-range": { type: 'string', default: "60" },
         "ping-floor": { type: 'boolean' },
@@ -95,58 +67,46 @@ const { values } = parseArgs({
 
 if (!values["follower-ping"] || !values["source-dsns"]) {
     logger("Missing required arguments: follower-ping and source-dsns must be provided.");
-    process.exit(1)
+    process.exit(1);
 }
 
-const MP_MysqlPing = new Map<string, MysqlPing>();
-const MP_FOLLOWER_NAME = values["follower-ping"]
-const MP_EXPORT_PORT: number = Number(values["export-port"])
-const MP_PING_RANGE: number = Number(values["ping-range"])
+const MP_FOLLOWER_NAME = values["follower-ping"];
+const MP_EXPORT_PORT: number = Number(values["export-port"]);
+const MP_PING_RANGE: number = Number(values["ping-range"]);
 const MP_PING_FLOOR: boolean = values["ping-floor"] || false;
-const MP_METRICS = new Map<string, number>()
+const MP_METRICS = new Map<string, number>();
+const MP_MysqlPing = new Map(values["source-dsns"].split(",").map(dsnStr => {
+    const dsn = parseConnectionString(dsnStr.trim());
+    logger(`Adding MySQL DSN ${dsn.name}`);
+    return [dsn.name, new MysqlPing(dsn, MP_PING_FLOOR)];
+}));
 
-values["source-dsns"]?.split(",")
-    .map(d => d.trim())
-    .forEach(fn => {
-        const dsn = parseConnectionString(fn)
-        logger(`Adding MySQL DSN ${dsn.name}`);
-
-        MP_MysqlPing.set(dsn.name, new MysqlPing(dsn, MP_PING_FLOOR))
-    })
-
-function http_server() {
-    Bun.serve({
-        port: MP_EXPORT_PORT,
-        async fetch(req) {
-            const url = new URL(req.url);
-            if (req.method === "GET" && url.pathname === "/metrics") {
-                let body = "# HELP mysqlping_timestamp created counter\n"
-                body += "# TYPE mysqlping_timestamp counter\n"
-                for (const [name, ts] of MP_METRICS.entries()) {
-                    const mmp = MP_MysqlPing.get(name)!
-                    body += `mysqlping_timestamp{mysql_name="${name}", mysql_addr="${mmp.getAddr()}", follower_name="${MP_FOLLOWER_NAME}"} ${ts}\n`
-                }
-                return new Response(body);
-            } else if (req.method === "GET" && url.pathname === "/ready") {
-                return new Response();
-            } else if (req.method === "POST" && url.pathname.startsWith("/ping")) {
-                const mysql_name = await req.text()
-                const mp_timestamp = MP_METRICS.get(mysql_name)
-
-                if (mp_timestamp === undefined) {
-                    return new Response(null, { status: 404 })
-                }
-                return new Response(mp_timestamp.toString());
+Bun.serve({
+    port: MP_EXPORT_PORT,
+    async fetch(req) {
+        const url = new URL(req.url);
+        if (req.method === "GET" && url.pathname === "/metrics") {
+            let body = "# HELP mysqlping_timestamp created counter\n# TYPE mysqlping_timestamp counter\n";
+            for (const [name, ts] of MP_METRICS.entries()) {
+                const mmp = MP_MysqlPing.get(name)!;
+                body += `mysqlping_timestamp{mysql_name="${name}", mysql_addr="${mmp.getAddr()}", follower_name="${MP_FOLLOWER_NAME}"} ${ts}\n`;
             }
-            return new Response(null, { status: 404 });
-        },
-    })
-}
+            return new Response(body);
+        } else if (req.method === "GET" && url.pathname === "/ready") {
+            return new Response();
+        } else if (req.method === "POST" && url.pathname.startsWith("/ping")) {
+            const mysql_name = await req.text();
+            const mp_timestamp = MP_METRICS.get(mysql_name);
+            return new Response(mp_timestamp?.toString() || null, { status: mp_timestamp ? 200 : 404 });
+        }
+        return new Response(null, { status: 404 });
+    },
+});
 
-async function main() {
-    const timestamp = getTimestamp()
+(async function main() {
+    const timestamp = getTimestamp();
     for (const md of MP_MysqlPing.values()) {
-        MP_METRICS.set(md.getName(), timestamp)
+        MP_METRICS.set(md.getName(), timestamp);
     }
 
     while (true) {
@@ -154,26 +114,21 @@ async function main() {
             if (!md.getIsPing()) {
                 md.setIsPing(true);
                 (async () => {
-                    const sleepTime = (Math.floor(Math.random() * MP_PING_RANGE) + 1) * 1000;
-                    logger(`${md.getName()} sleeping for ${sleepTime} ms`);
-                    await sleep(sleepTime)
+                    await sleep((Math.floor(Math.random() * MP_PING_RANGE) + 1) * 1000);
                     try {
-                        const timestamp = getTimestamp()
-                        await md.ping(timestamp)
-                        MP_METRICS.set(md.getName(), timestamp + (getTimestamp() - timestamp))
+                        const timestamp = getTimestamp();
+                        await md.ping(timestamp);
+                        MP_METRICS.set(md.getName(), getTimestamp());
                     } catch (error) {
-                        logger(`${md.getName()} error: ${error}`)
+                        logger(`${md.getName()} error: ${error}`);
                     } finally {
                         md.setIsPing(false);
                     }
                 })();
             } else {
-                logger(md.getName() + " running")
+                logger(md.getName() + " running");
             }
         }
-        await sleep(1000)
+        await sleep(1000);
     }
-}
-
-main()
-http_server()
+})();
