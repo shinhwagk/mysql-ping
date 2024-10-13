8f4fe1ceeb0e0b222e16b1b9c994b010636dbf1e
gk <shinhwagk@outlook.com>
2024-10-13 12:03:48 +0800

    Update mysqlping_client.ts

--- mysqlping_client.ts
+++ mysqlping_client.ts
@@ -22,30 +22,31 @@ const MP_FOLLOWER_ADDRS: string[] = followerAddrs.split(",").map(fa => fa.trim()
 const MP_MYSQL_NAME = mysqlName;
 const MP_PING_RANGE: number = parseInt(pingRange, 10);
 
+// exit 2 follower error
+// exit 1 mysql down
+// exit 0 mysql live
 (async () => {
     for (const fAddr of MP_FOLLOWER_ADDRS) {
         try {
             if (!(await fetch(`http://${fAddr}/ready`)).ok) {
                 console.error(`Follower ${fAddr} not ready.`);
-                process.exit(1)
+                process.exit(2)
             }
 
             const res = await fetch(`http://${fAddr}/ping`, { method: "POST", body: MP_MYSQL_NAME });
             if (res.ok) {
                 const pingTimestamp = parseInt(await res.text(), 10);
                 if (getTimestamp() - pingTimestamp <= MP_PING_RANGE) {
-                    console.log("alive");
                     process.exit(0);
                 }
             } else {
                 console.error(`Ping to follower ${fAddr} returned non-ok status.`);
-                process.exit(1)
+                process.exit(2)
             }
         } catch (error) {
             console.error(`Error fetching from ${fAddr}: ${error}`);
-            process.exit(1)
+            process.exit(2)
         }
     }
-    console.log("down");
-    process.exit(0);
+    process.exit(1);
 })();
