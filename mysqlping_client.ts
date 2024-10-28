7c682a4a989d0eae0437352179b5bbb485e42244
xu zhang <shinhwagk@outlook.com>
2024-10-28 18:12:23 +0800

    0.2.24

--- mysqlping_client.ts
+++ mysqlping_client.ts
@@ -11,19 +11,18 @@ if (!MPC_ARGS_PING_ADDRS || !MPC_ARGS_MYSQL_ADDR) {
 const pingAddrs: string[] = MPC_ARGS_PING_ADDRS.split(',').filter((a: string) => a.length >= 1).map((fa: string) => fa.trim());
 
 // exit 2 ping error, exit 1 mysql down, exit 0 mysql live
-try {
-    for (const pingAddr of pingAddrs) {
+for (const pingAddr of pingAddrs) {
+    try {
         if (!(await fetch(`http://${pingAddr}/ready`)).ok) throw new Error(`ping-addr: ${pingAddr}, Not ready`);
         const res = await fetch(`http://${pingAddr}/ping?mysql_addr=${MPC_ARGS_MYSQL_ADDR}`);
         if (res.ok) Deno.exit(0);
-        if (res.status == 404) throw new Error(`ping-addr:${pingAddr}, mysql-addr: ${MPC_ARGS_MYSQL_ADDR}, status:${res.status}, not exists`);
-        if (res.status == 599) {
-            console.error(`ping-addr:${pingAddr}, mysql-addr: ${MPC_ARGS_MYSQL_ADDR}, status:${res.status}, down`);
-            Deno.exit(1);
-        }
-        throw new Error(`ping-addr:${pingAddr}, mysql-addr: ${MPC_ARGS_MYSQL_ADDR}, status:${res.status}, unknown`);
+        else if (res.status == 404) throw new Error(`ping-addr:${pingAddr}, mysql-addr: ${MPC_ARGS_MYSQL_ADDR}, status:${res.status}, not exists`);
+        else if (res.status == 599) console.error(`ping-addr:${pingAddr}, mysql-addr: ${MPC_ARGS_MYSQL_ADDR}, status:${res.status}, down`);
+        else throw new Error(`ping-addr:${pingAddr}, mysql-addr: ${MPC_ARGS_MYSQL_ADDR}, status:${res.status}, unknown`);
+    } catch (error) {
+        console.error(`${error}`);
+        Deno.exit(2);
     }
-} catch (error) {
-    console.error(`${error}`);
-    Deno.exit(2);
 }
+
+Deno.exit(1);
